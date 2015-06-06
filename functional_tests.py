a455845b0df1dc278bc8217f29a0a658631642c8
Lundy Bernard <lundy.bernard@gmail.com>
2015-06-05 20:43:53 -0500

    Added a user story outline to my functional tests. follow the white rabbit Alice

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,32 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# Lets practice some end-to-end user story writing!
+
+# Alice visits our site
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# she notices the page title and headder mention "To-Do" lists
+assert 'To-Do' in browser.title
+
+# she is invited to enter a to-do item immediately
+
+# she types in "Follow the white rabbit" into a text box
+
+# when she hits 'Enter' the page updates, and now lists
+# "1: Follow the white rabbit" as an item in a to-do list
+
+# there is still a text box inviting her to add another item
+#she enters "obey the testing goat"
+
+# the page updates again and now shows both items on her list
+
+# Alice woners if the site will remember her list
+# she sees that the site has generated a unique URL for her
+# and there is some text explaining its use
+
+# she visits that URL and her list is there.
+
+# satisfied, she moves on to other adventures
+browser.quit()
