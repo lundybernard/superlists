daff6c3a14502636e6443bd99a29425cb3ec6f3e
Lundy Bernard <lundy.bernard@gmail.com>
2015-06-07 20:40:06 -0500

    functional test now checks that can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 # Lets practice some end-to-end user story writing!
@@ -19,17 +20,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # she notices the page title and headder mention "To-Do" lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         self.fail('Finish the test!')
 
         # she is invited to enter a to-do item immediately
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'enter a to-do item'
+        )
 
         # she types in "Follow the white rabbit" into a text box
+        input.send_keys('Follow the white rabbit')
 
         # when she hits 'Enter' the page updates, and now lists
         # "1: Follow the white rabbit" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tab_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Follow the white rabbit' for row in rows)
+        )
 
         # there is still a text box inviting her to add another item
         # she enters "obey the testing goat"
+        self.fail('Finish the test!')
 
         # the page updates again and now shows both items on her list
 
