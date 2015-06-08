36b52e82da0af84be964e9d1b78918ce0bbdbcd7
Lundy Bernard <lundy.bernard@gmail.com>
2015-06-08 18:27:57 -0500

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -22,26 +22,26 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        self.fail('Finish the test!')
 
         # she is invited to enter a to-do item immediately
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'enter a to-do item'
+            'Enter a to-do item'
         )
 
         # she types in "Follow the white rabbit" into a text box
-        input.send_keys('Follow the white rabbit')
+        inputbox.send_keys('Follow the white rabbit')
 
         # when she hits 'Enter' the page updates, and now lists
         # "1: Follow the white rabbit" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tab_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Follow the white rabbit' for row in rows)
+            any(row.text == '1: Follow the white rabbit' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # there is still a text box inviting her to add another item
