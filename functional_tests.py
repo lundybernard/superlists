20d4b8ba2eec788433370eea9dfb583e6598bb76
Lundy Bernard <lundy.bernard@gmail.com>
2015-06-10 18:38:43 -0500

    DRY up functional_tests with check_for_rows... helper function

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Alice visits our site
         self.browser.get('http://localhost:8000')
@@ -40,9 +45,7 @@ class NewVisitorTest(unittest.TestCase):
         import time
         time.sleep(10)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Follow the white rabbit', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Follow the white rabbit')
 
         # there is still a text box inviting her to add another item
         # she enters "obey the testing goat"
@@ -51,11 +54,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # the page updates again and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
 
-        self.assertIn('1: Follow the white rabbit', [row.text for row in rows])
-        self.assertIn('2: Obey the testing goat', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Follow the white rabbit')
+        self.check_for_row_in_list_table('2: Obey the testing goat')
 
         # Alice woners if the site will remember her list
         # she sees that the site has generated a unique URL for her
