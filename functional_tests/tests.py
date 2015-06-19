bb9f0091e063c9f9bd0b9750516270fa7580981e
Lundy Bernard <lundy.bernard@gmail.com>
2015-06-18 20:37:45 -0500

    use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -7,7 +7,7 @@ import time
 # Lets practice some end-to-end user story writing!
 # now with 100% more LiveServerTestCase from django.test 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Firefox()
