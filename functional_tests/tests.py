a6ec4328649deed63b178418ca1437646733318d
Lundy Bernard <lundy.bernard@gmail.com>
2015-06-21 16:24:09 -0500

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,12 +2,27 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+import sys
 import time
 
 # Lets practice some end-to-end user story writing!
 # now with 100% more LiveServerTestCase from django.test 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
     
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -23,7 +38,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Alice goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
@@ -46,7 +61,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Alice visits our site
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # she notices the page title and headder mention "To-Do" lists
         self.assertIn('To-Do', self.browser.title)
@@ -96,7 +111,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # Bob visits the home page.
         # there is no sign of Alice's list
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Follow the white rabbit', page_text)
         self.assertNotIn('Ovey the testing goat', page_text)
