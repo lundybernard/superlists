0a11d87ade905b8dc93730ee9af869014aae1658
Lundy Bernard <lundy.bernard@gmail.com>
2015-07-02 18:48:22 -0500

    rename all item input ids and names. still broken

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -2,8 +2,9 @@ from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+import time
 
-def NewVisitorTest(FunctionalTest):
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Alice visits our site
@@ -15,7 +16,7 @@ def NewVisitorTest(FunctionalTest):
         self.assertIn('To-Do', header_text)
 
         # she is invited to enter a to-do item immediately
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -36,7 +37,7 @@ def NewVisitorTest(FunctionalTest):
 
         # there is still a text box inviting her to add another item
         # she enters "obey the testing goat"
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Obey the testing goat')
         inputbox.send_keys(Keys.ENTER)
 
@@ -63,7 +64,7 @@ def NewVisitorTest(FunctionalTest):
         self.assertNotIn('Ovey the testing goat', page_text)
 
         # Bob starts a new list by entering a new item
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
