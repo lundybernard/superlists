4534e0c37cf015998c27ae2e66f29203f8481253
Lundy Bernard <lundy.bernard@gmail.com>
2015-07-18 16:20:14 -0500

    Implements duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,20 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def test_cannot_add_duplicate_items(self):
+        # Alice goes to the home page and starts a new list
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Buy Wellies\n')
+        self.check_for_row_in_list_table('1: Buy Wellies')
+
+        # She accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Buy Wellies\n')
+
+        # She sees a helpful error message
+        self.check_for_row_in_list_table('1: Buy Wellies')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You've already got this in your list")
+
     def test_cannot_add_empty_list_items(self):
         # Alice goes to the home page and accidently tries to submit
         # an empty list item. she hits enter on the empty input box
